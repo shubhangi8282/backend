fe20689c55cf1e3ac6d4f16f74d5fda56bf54b57
shubhangi8282 <61152246+shubhangi8282@users.noreply.github.com>
2022-09-21 17:17:20 +0530

    fixes
    - bump jvm version on pom
    - update to the jdbc url for connecting to the db

--- productwebservice/src/main/java/com/quinnox/productwebservice/ConnectionFactory.java
+++ productwebservice/src/main/java/com/quinnox/productwebservice/ConnectionFactory.java
@@ -9,9 +9,9 @@ public class ConnectionFactory {
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException
     {
-        String url = "jdbc:mysql://localhost:3306/demo2";
-        String user = "root";
-        String password = "root";
+		String url = "database-1.co0gkoh2z93x.ap-south-1.rds.amazonaws.com";
+        String user = "admin";
+        String password = "QWEqwe12";
     
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection    con = DriverManager.getConnection(url, user, password);
